51a65790b176d6beefbd832f5f5ce98fb4c3ed93
Venrique <00046517@uca.edu.sv>
2024-01-31 17:32:50 -0600

    finishing list entry multilingual

--- mod/data/lib.php
+++ mod/data/lib.php
@@ -718,7 +718,7 @@ class data_field_base {     // Base class for Database Field Types (see field/*/
             }
         }
 
-        $str = format_text($content->content, $content->content1, $options);
+        $str = format_text(' ', $content->content1, $options);
         return $str;
     }
 
