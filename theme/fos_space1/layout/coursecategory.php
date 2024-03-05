c5877da99ada74daf4081bccf96e7db884700092
Venrique <00046517@uca.edu.sv>
2024-03-05 17:14:11 -0600

    category buttons dynamic

--- theme/fos_space1/layout/coursecategory.php
+++ theme/fos_space1/layout/coursecategory.php
@@ -16,7 +16,7 @@ if (isloggedin() && !isguestuser()) {
     $courseModulesObject = get_fast_modinfo($course->id);
     $courseModules = $courseModulesObject->get_cms();
 
-    echo $courseModules[1]->type;
+    print_object($courseModulesObject);
 
 
     // Add block button in editing mode.
