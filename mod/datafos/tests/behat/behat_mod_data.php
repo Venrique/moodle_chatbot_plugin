f72101e3ac04cd539f8470a963f4b4cf06b8959c
Maybelline Portillo <portillom634@gmail.com>
2024-10-04 20:59:24 -0600

    update datafos

--- mod/datafos/tests/behat/behat_mod_data.php
+++ mod/datafos/tests/behat/behat_mod_data.php
@@ -50,7 +50,7 @@ class behat_mod_data extends behat_base {
     public function i_add_a_field_to_database_and_i_fill_the_form_with($fieldtype, $activityname, TableNode $fielddata) {
         $this->execute('behat_navigation::i_am_on_page_instance', [$this->escape($activityname), 'datafos activity']);
 
-        $fieldsstr = get_string('fields', 'mod_datafos');
+        $fieldsstr = get_string('fields', 'data');
 
         $this->execute("behat_navigation::i_navigate_to_in_current_page_administration", $fieldsstr);
         $this->execute('behat_general::i_click_on', [get_string('newfield', 'mod_datafos'), "button"]);
